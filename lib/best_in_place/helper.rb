13a5cfcc4b68e46c7dffea1b43eac66d3212d393
Burkhard Vogel-Kreykenbohm <b.vogel@buddyandselly.com>
2013-01-30 20:30:06 +0100

    add DisplayMethods even for empty values, fix for #229

--- lib/best_in_place/helper.rb
+++ lib/best_in_place/helper.rb
@@ -86,7 +86,7 @@ module BestInPlace
 
   private
     def build_value_for(object, field, opts)
-      return "" if object.send(field).blank?
+      is_blank = object.send(field).blank?
 
       klass = if object.respond_to?(:id)
         "#{object.class}_#{object.id}"
@@ -96,12 +96,11 @@ module BestInPlace
 
       if opts[:display_as]
         BestInPlace::DisplayMethods.add_model_method(klass, field, opts[:display_as])
-        object.send(opts[:display_as]).to_s
+        object.send(opts[:display_as]).to_s unless is_blank
 
       elsif opts[:display_with].try(:is_a?, Proc)
         BestInPlace::DisplayMethods.add_helper_proc(klass, field, opts[:display_with])
-        opts[:display_with].call(object.send(field))
-
+        opts[:display_with].call(object.send(field)) unless is_blank
       elsif opts[:display_with]
         BestInPlace::DisplayMethods.add_helper_method(klass, field, opts[:display_with], opts[:helper_options])
         if opts[:helper_options]
@@ -110,6 +109,8 @@ module BestInPlace
           BestInPlace::ViewHelpers.send(opts[:display_with], object.send(field))
         end
 
+      elsunless is_blank
+        object.send(field).to_s.presence || ''
       else
         object.send(field).to_s
       end
